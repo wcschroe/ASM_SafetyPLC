fd6f9ab20a55cb6d8bb44f0079b245b7c010d65c
William Schroeder <william.schroeder78@gmail.com>
2019-08-16 22:57:18 -0700

    F O R   L O O P S   B R O T H E R

--- ASM_SafetyPLC.ino
+++ ASM_SafetyPLC.ino
@@ -2,8 +2,8 @@
 #define contactorEnable A5
 #define EMO_OUT_0 4
 #define EMO_OUT_1 5
-#define Door_SW1_OUT 7
-#define Door_SW2_OUT 6
+#define Door_SW1_OUT 6
+#define Door_SW2_OUT 7
 	// external door switch exicitaion....#define Door_SW2_OUT, OUTPUT);
 //Inputs
 #define EMO_IN_0 A0
@@ -13,14 +13,14 @@
 #define Main_Controller_ON 2
 
 bool safe = false;
-unsigned long wiringCheckInterval = 10000;
 
 uint16_t INPUTPINS[] = { 
 	EMO_IN_0, 
 	EMO_IN_1, 
 	Door_SW1_IN, 
-	Door_SW2_IN 
+	Door_SW2_IN
 };
+
 uint16_t OUTPUTPINS[] = { 
 	EMO_OUT_0, 
 	EMO_OUT_1, 
@@ -31,28 +31,22 @@ uint16_t OUTPUTPINS[] = {
 int numInputs = sizeof(INPUTPINS) / sizeof(INPUTPINS[0]);
 int numOutputs = sizeof(OUTPUTPINS) / sizeof(OUTPUTPINS[0]);
 
+void SHUTDOWN() {
+	while (true) {
+		digitalWrite(contactorEnable, LOW);
+	}
+}
+
 void digitalWriteOutputs(uint8_t val) {
-	digitalWrite(EMO_OUT_0, val);
-	digitalWrite(EMO_OUT_1, val);
-	digitalWrite(Door_SW1_OUT, val);
-	digitalWrite(Door_SW2_OUT, val);
+	for (int i = 0; i < numOutputs; i++) digitalWrite(OUTPUTPINS[i], val);
 }
 
 void pinSetup() {
 	//Outputs
-	pinMode(contactorEnable, OUTPUT);
-	pinMode(EMO_OUT_0, OUTPUT);
-	pinMode(EMO_OUT_1, OUTPUT);
-	pinMode(Door_SW1_OUT, OUTPUT);
-	pinMode(Door_SW2_OUT, OUTPUT);
-
+	for (int i = 0; i < numOutputs; i++) pinMode(OUTPUTPINS[i], OUTPUT);
 	
 	//Inputs
-	pinMode(EMO_IN_0, INPUT);
-	pinMode(EMO_IN_1, INPUT);
-	pinMode(Door_SW1_IN, INPUT);
-	pinMode(Door_SW2_IN, INPUT);
-	pinMode(Main_Controller_ON, INPUT);
+	for (int i = 0; i < numInputs; i++) pinMode(INPUTPINS[i], INPUT);
 
 	digitalWriteOutputs(HIGH);
 }
@@ -73,21 +67,24 @@ bool safeCheck() {
 	//good to scan and store all switch states for error output to user to know which switches are in fault   (amybe in bit set to one int?)
 
 	if (safe) return true;
-	else return false;
+	else SHUTDOWN();
 }
 
-void wiringCheck() {
-
+bool wiringCheck() {
+	for(int i = 0; i < numOutputs; i++) {
+		digitalWrite(OUTPUTPINS[i], LOW);
+		delay(100);
+		if (digitalRead(INPUTPINS[i]) != LOW) SHUTDOWN();
+		digitalWrite(OUTPUTPINS[i], HIGH);
+	}
 }
 
 void setup() {
 	pinSetup();
+	wiringCheck();
+	digitalWrite(contactorEnable, HIGH);
 } 
 
 void loop() {
-	if (safeCheck()) digitalWrite(contactorEnable, HIGH);
-	else {
-		digitalWrite(contactorEnable, LOW);
-		while (true) safeCheck();
-	}
+	safeCheck();
 }
